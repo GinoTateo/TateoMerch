f56d4797c661fa607c48c359c72a23496bdd3f35
Gino Tateo <47284697+GinoTateo@users.noreply.github.com>
2022-12-19 18:18:20 -0800

    more render requirement files

--- MerchManagerV1/settings.py
+++ MerchManagerV1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6_42+fg2^z+-_qt#38uvn)6vb+$8#!&omfdq+-p_#bnryq*@(t'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','tateomerch.herokuapp.com','TateoMerch.com']
+ALLOWED_HOSTS = ['127.0.0.1','tateomerch.onrender.com','TateoMerch.com']
 
 # Application definition
 
@@ -84,6 +85,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
