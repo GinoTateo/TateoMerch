c278106d68a7b77b8bbbc3c6c03ee6432855f7dc
ginotateo <tateogino@gmail.com>
2024-02-07 23:47:40 -0800

    Debug

--- MerchManagerV1/settings.py
+++ MerchManagerV1/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://shipteo.onrender.com/', 'shipteo.onrender.com', 'shipteo.com', 'https://www.shipteo.com/', '*']
 
